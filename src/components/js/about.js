ea7ebee81fbe9260252c3777453be9cffdb8e325
Aisha <aisha.kapan@nodelondon.com>
2022-12-14 15:05:08 +0000

    added about paragraphs

--- src/components/js/about.js
+++ src/components/js/about.js
@@ -2,7 +2,9 @@ function About() {
     return (
     <div>
         <h1>About</h1>
+        <p>Reclusive charismatic bachelor and castle owner investing in real estate overseas. Any enquiries must be negotiated through my personal agent Knock.</p>
         <h1>Interests</h1>
+        <p>Travel, rats, coffins, beautiful maidens, blood letting, black arts.</p>
     </div>
     );
 }
