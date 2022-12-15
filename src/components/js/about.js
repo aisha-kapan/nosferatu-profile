b5513b74abb5fc4a30b399f83a33434551fa9270
Aisha <aisha.kapan@nodelondon.com>
2022-12-15 16:41:08 +0000

    Asdjusted colors and alignment

--- src/components/js/about.js
+++ src/components/js/about.js
@@ -3,6 +3,7 @@ function About() {
     <div className="About">
         <h3>About</h3>
         <p>Reclusive charismatic bachelor and castle owner investing in real estate overseas. Any enquiries must be negotiated through my personal agent Knock.</p>
+        <br></br>
         <h3>Interests</h3>
         <p>Travel, rats, coffins, beautiful maidens, blood letting, black arts.</p>
     </div>
