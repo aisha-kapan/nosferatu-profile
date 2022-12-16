6084b4a80c2b4dc67b5bc749e7e9a7d9a4b55a6a
Aisha <aisha.kapan@nodelondon.com>
2022-12-16 12:06:59 +0000

    installed gsap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { gsap } from "gsap";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -14,4 +15,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+gsap.to(".Bat", {rotation: 27, x: 100, duration: 1});
 reportWebVitals();
