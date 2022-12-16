dde4bca90e306e3589490e4f70c0231954f4087b
Aisha <aisha.kapan@nodelondon.com>
2022-12-16 16:22:26 +0000

    name typo

--- src/components/js/title.js
+++ src/components/js/title.js
@@ -1,7 +1,7 @@
 function Title() {
     return (
     <div className="Title">
-        <h1>Count Orlock</h1>
+        <h1>Count Orlok</h1>
         <h5>"The Bird of Death"</h5>
         <a href="https://www.nosferatumovie.com/">thecreationofbelial.de</a>
     </div>
